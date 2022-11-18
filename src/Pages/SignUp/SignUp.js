cb8c2cd4a3eb484e965d4d37010abed370ae4c0c
SamitMahadi <samithossainmahadi@gmail.com>
2022-11-18 06:31:20 -0500

    verify token done

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -26,7 +26,7 @@ const SignUp = () => {
                 }
                 updateUser(userInfo)
                     .then(() => {
-                        saveUser(data.name,data.email)
+                        saveUser(data.name, data.email)
                     })
                     .catch(err => console.error(err))
             })
@@ -37,21 +37,32 @@ const SignUp = () => {
     }
 
 
-const saveUser=(name,email)=>{
-    const user ={name,email}
-    fetch('http://localhost:5000/users',{
-        method: 'POST',
-        headers: {
-            'content-type':'application/json'
-        },
-        body: JSON.stringify(user)
-    })
-    .then(res=>res.json())
-    .then(data=>{
-        console.log('save user', data);
-        navigate('/')
-    })
-}
+    const saveUser = (name, email) => {
+        const user = { name, email }
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                getUserToken(email)
+            })
+    }
+
+    const getUserToken = email => {
+        fetch(`http://localhost:5000/jwt?email=${email}`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.accessToken) {
+                    localStorage.setItem('accessToken', data.accessToken)
+                    navigate('/')
+                }
+            })
+    }
+
 
     return (
         <div>
